23d1198058ddae6040dd490981931c6b3a2e9f8b
Sattwik Sahu <sahusattwik@gmail.com>
2022-11-02 23:03:17 +0530

    Created .gitignore

--- src/util/cvscroll.py
+++ src/util/cvscroll.py
@@ -4,6 +4,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import pyautogui
+from util.mouse_action_handler import MouseActionHandler
 
 
 mp_drawing = mp.solutions.drawing_utils
@@ -30,16 +31,19 @@ def distance(x1, y1, x2, y2) -> float:
     return np.linalg.norm((x2 - x1, y2 - y1))
 
 
-def capture_video(touch_threshold = 50, scroll_speed=8, debug=False):
+def capture_video(touch_threshold=50, scroll_speed=8, debug=False):
     capture = cv2.VideoCapture(0)
     hand_detector = mp.solutions.hands.Hands(max_num_hands=1)
     drawing_utils = mp.solutions.drawing_utils
     screen_width, screen_height = pyautogui.size()
+
     def fingers_touch(tip1, tip2, threshold=20) -> bool:
         return distance(tip1.x * screen_width, tip1.y * screen_height,
-        tip2.x * screen_width, tip2.y * screen_height) < threshold
-    scrolling = False
-    moving = True
+                        tip2.x * screen_width, tip2.y * screen_height) < threshold
+
+    # The mouse action handler
+    m_handler = MouseActionHandler(scroll_speed=scroll_speed)
+
     while capture.isOpened():
         _, frame = capture.read()
         frame = cv2.flip(frame, 1)
@@ -48,6 +52,7 @@ def capture_video(touch_threshold = 50, scroll_speed=8, debug=False):
         hand_output = hand_detector.process(rgb_frame)
         hand_landmarks = hand_output.multi_hand_landmarks
         if hand_landmarks:
+            # Plotting the hand connections
             for hand in hand_landmarks:
                 if debug:
                     drawing_utils.draw_landmarks(
@@ -64,14 +69,18 @@ def capture_video(touch_threshold = 50, scroll_speed=8, debug=False):
                             radius=int(touch_threshold * 0.5), color=(10, 200, 255), thickness=4)
 
                 # Move mouse
-                if moving:
-                    palm_borders = [hand.landmark[i] for i in [5, 9, 13, 17, 0]]
-                    palm_center_mean_pos = np.mean([(base.x, base.y) for base in palm_borders], axis=0)
+                if not m_handler.mouse_lock:
+                    palm_borders = [hand.landmark[i]
+                                    for i in [5, 9, 13, 17, 0]]
+                    palm_center_mean_pos = np.mean(
+                        [(base.x, base.y) for base in palm_borders], axis=0)
                     mx, my = pyautogui.position()
                     stabz = 0.5
                     pyautogui.moveTo(
-                        (palm_center_mean_pos[0] * screen_width * (1 - stabz) + mx * stabz) * 1.1,
-                        (palm_center_mean_pos[1] * screen_height * (1 - stabz) + my * stabz) * 1.1
+                        (palm_center_mean_pos[0] * screen_width *
+                         (1 - stabz) + mx * stabz) * 1.1,
+                        (palm_center_mean_pos[1] * screen_height *
+                         (1 - stabz) + my * stabz) * 1.1
                     )
                     if debug:
                         draw_circle_on_landmark(
@@ -80,41 +89,56 @@ def capture_video(touch_threshold = 50, scroll_speed=8, debug=False):
                             color=(230, 70, 20), thickness=2
                         )
 
+                # Handle click gestures
+                m_handler.update(
+                    ges_left=fingers_touch(
+                        thumb_tip, index_tip, threshold=touch_threshold),
+                    ges_middle=fingers_touch(
+                        thumb_tip, hand.landmark[12], threshold=touch_threshold),
+                    ges_right=fingers_touch(
+                        thumb_tip, hand.landmark[16], threshold=touch_threshold)
+                )
+
                 # Thumb + index == Lt-click
                 if fingers_touch(thumb_tip, index_tip, threshold=touch_threshold):
-                    if moving:
+                    if not m_handler.mouse_lock:
                         print('Left click!', index_tip.x, index_tip.y)
-                        pyautogui.click()
-                        pyautogui.sleep(1)
+                        # pyautogui.click()
+                        # pyautogui.sleep(1)
                     else:
-                        pyautogui.scroll(-scroll_speed)
+                        # pyautogui.scroll(-scroll_speed)
+                        pass
                 # Thumb + ring == Rt-click
                 if fingers_touch(thumb_tip, hand.landmark[16], threshold=touch_threshold):
-                    if moving:
+                    if not m_handler.mouse_lock:
                         print('Right click!', index_tip.x, index_tip.y)
-                        pyautogui.click(button='right')
-                        pyautogui.sleep(1)
+                        # pyautogui.click(button='right')
+                        # pyautogui.sleep(1)
                     else:
-                        pyautogui.scroll(scroll_speed)
+                        # pyautogui.scroll(scroll_speed)
+                        pass
 
                 # Thumb + middle == middle click (toggle scrolling | ONLY in moving mode)
                 if fingers_touch(thumb_tip, hand.landmark[12], threshold=touch_threshold):
-                    if not moving:
+                    if m_handler.mouse_lock:
                         print('EXIT')
                         capture.release()
-                    elif not scrolling:
-                        scrolling = True
-                        pyautogui.mouseDown(button='middle')
+                    elif not m_handler.mouse_lock:
+                        # scrolling = True
+                        # pyautogui.mouseDown(button='middle')
                         print("Scrolling now...")
-                elif scrolling:
-                    scrolling = False
-                    pyautogui.mouseUp(button='middle')
-                    print('Scrolling end')
+                elif m_handler.mouse_lock:
+                    pass
+                    # scrolling = False
+                    # pyautogui.mouseUp(button='middle')
+                    # print('Scrolling end')
 
-                # Thumb + pinky == toggle mouse movement
+                    # Thumb + pinky == toggle mouse movement
                 if fingers_touch(thumb_tip, hand.landmark[20], threshold=touch_threshold):
-                    moving = not moving
-                    print(f"Mouse {'UNLOCKED' if moving else 'LOCKED'}")
+                    # moving = not moving
+                    m_handler.toggle_mouse_lock()
+                    print(
+                        f"Mouse {'UNLOCKED' if not m_handler.mouse_lock else 'LOCKED'}")
                     sleep(0.5)
         if debug:
             cv2.imshow('Front Camera Feed', frame)
--- src/util/mouse_action_handler.py
+++ src/util/mouse_action_handler.py
@@ -10,8 +10,8 @@ class MouseActionHandler:
     def __init__(self, scroll_speed: int) -> None:
         self.scroll_speed = scroll_speed
         self.left = self.right = self.middle = False
-        self.moving = True
-        self.scrolling = False
+        self.mouse_lock = True
+        # self.scrolling = False
 
     def update(self, ges_left: bool, ges_middle: bool, ges_right: bool):
         """
@@ -20,19 +20,19 @@ class MouseActionHandler:
         """
         if ges_left and not self.left:
             self.left = True
-            if self.moving:
+            if not self.mouse_lock:
                 pyautogui.mouseDown(button='left')
             else:
                 pyautogui.scroll(-self.scroll_speed)
         elif ges_middle and not self.middle:
             self.middle = True
-            if self.moving:
+            if not self.mouse_lock:
                 pyautogui.mouseDown(button='middle')
             else:
                 pyautogui.scroll(self.scroll_speed)
         elif ges_right and not self.right:
             self.right = True
-            if self.moving:
+            if not self.mouse_lock:
                 pyautogui.mouseDown(button='right')
 
         if not ges_left and self.left:
@@ -45,8 +45,8 @@ class MouseActionHandler:
             self.right = False
             pyautogui.mouseUp(button='right')
 
-    def toggle_mouse_mode(self):
+    def toggle_mouse_lock(self):
         """
-        Toggles the mouse mode between MOVE & SCROLL
+        Toggles the mouse lock
         """
-        self.moving = not self.moving
+        self.mouse_lock = not self.mouse_lock
